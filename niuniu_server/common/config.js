c099199352f1135b97f1d32dec6a1ac8d62184ef
NL <184674881@qq.com>
2019-07-20 11:54:41 +0800

    修改前后端端口对应错误

--- niuniu_server/common/config.js
+++ niuniu_server/common/config.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-
+//
 exports.servers =
     {
         "center_login": {
@@ -7,17 +7,17 @@ exports.servers =
                 "1.0.0": { "isHot": 1,"hotUrl": "http://47.104.255.159/renheniuniu"}
             },
             "version": "1.0.0",
-            "servers": [
+            "servers": [//loginserver
                 { "index": 1,"desc": "renheniuniu", "name":"内测版","url": "127.0.0.1:7002", "token": "fKhxfw/cwLE2ok2VrqmV9w==" },
             ],
             "recommend": 1
         },
         "login_hall": {
-            "servers":
+            "servers"://hall_server
                 { "index": 1,"desc": "renheniuniu", "url": "127.0.0.1:7003","weChat":"CSDN7777", "shareWeb": "http://172.20.10.3/download/index.html" }
         },
         "hall_game": {
             "servers":
-                { "index": 1, "desc": "renheniuniu", "url": "127.0.0.1:7003" }
+                { "index": 1, "desc": "renheniuniu", "url": "127.0.0.1:10001" }
         }
     }
